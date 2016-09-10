0cea57a61497ee254ba23f9e4585131d6aef7712
Legendary <gregorius.stahlbart@gmail.com>
2016-09-10 17:04:06 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -14,7 +14,6 @@ apt-get update
 apt-get install linux-image-extra-$(uname -r)
 apt-get update
 apt-get install docker-engine
-service docker status
 
 curl -L https://github.com/docker/compose/releases/download/1.8.0/docker-compose-`uname -s`-`uname -m` > /usr/local/bin/docker-compose
 chmod +x /usr/local/bin/docker-compose
