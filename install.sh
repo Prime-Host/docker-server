2e52486314e3e20466bd601cf689c1c90a620d52
Nurian <gregorius.stahlbart@gmail.com>
2017-03-31 15:55:20 +0200

    add new compose version

--- install.sh
+++ install.sh
@@ -16,7 +16,7 @@ apt-get install linux-image-extra-$(uname -r) -y
 apt-get update
 apt-get install docker-engine -y
 
-curl -L https://github.com/docker/compose/releases/download/1.8.0/docker-compose-`uname -s`-`uname -m` > /usr/local/bin/docker-compose
+curl -L https://github.com/docker/compose/releases/download/1.12.0-rc2/docker-compose-`uname -s`-`uname -m` > /usr/local/bin/docker-compose
 chmod +x /usr/local/bin/docker-compose
 
 echo "alias dps='docker ps --format "table {{.ID}}\t{{.Names}}\t{{.Image}}\t{{.Status}}\t{{.Size}}" -sa'" >> /root/.zsh_aliases
