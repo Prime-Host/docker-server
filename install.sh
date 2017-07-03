b12634fb56b94a807347e8936f887a403adc9b2e
Nurian <gregorius.stahlbart@gmail.com>
2017-07-03 15:43:16 +0200

    new version of docker and docker-compose

--- install.sh
+++ install.sh
@@ -1,24 +1,21 @@
 #!/bin/bash
+
+# install basiscs
 apt-get update
 apt-get upgrade -y
 apt-get install vim htop iftop git curl zsh -y
+
+# install oh my zsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 
-apt-get update
-apt-get install apt-transport-https ca-certificates
-apt-key adv --keyserver hkp://p80.pool.sks-keyservers.net:80 --recv-keys 58118E89F3A912897C070ADBF76221572C52609D
-echo "deb https://apt.dockerproject.org/repo ubuntu-xenial main" >> /etc/apt/sources.list.d/docker.list
-apt-get update
-apt-get purge lxc-docker
-apt-cache policy docker-engine
-apt-get update
-apt-get install linux-image-extra-$(uname -r) -y
-apt-get update
-apt-get install docker-engine -y
+# install docker version with rancher support
+curl https://releases.rancher.com/install-docker/17.03.sh | sh
 
-curl -L https://github.com/docker/compose/releases/download/1.12.0-rc2/docker-compose-`uname -s`-`uname -m` > /usr/local/bin/docker-compose
+# install docker-compose
+curl -L https://github.com/docker/compose/releases/download/1.14.0/docker-compose-`uname -s`-`uname -m` > /usr/local/bin/docker-compose
 chmod +x /usr/local/bin/docker-compose
 
+# alias for better overview
 echo "alias dps='docker ps --format "table {{.ID}}\t{{.Names}}\t{{.Image}}\t{{.Status}}\t{{.Size}}" -sa'" >> /root/.zsh_aliases
 
 echo "export HISTFILESIZE=" >> /root/.bashrc
