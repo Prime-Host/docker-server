bef18b041ef7bbe45c75f241fb52fa141b26b71c
root <root@vmd22754.contaboserver.net>
2017-10-23 16:27:03 +0200

    bug fixes

--- install.sh
+++ install.sh
@@ -5,10 +5,6 @@ apt-get update
 apt-get dits-upgrade -y
 apt-get install vim htop iftop git curl zsh -y
 
-# install oh my zsh with bira theme
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
-sed -i -- 's/robbyrussell/bira/g' /root/.zshrc
-
 # install docker version with rancher support
 apt-get install docker-ce=17.06.2~ce-0~ubuntu
 
@@ -17,7 +13,11 @@ curl -L https://github.com/docker/compose/releases/download/1.16.1/docker-compos
 chmod +x /usr/local/bin/docker-compose
 
 # alias for better overview
-echo  "alias dps='docker ps --format "table {{.ID}}\t{{.Names}}\t{{.Image}}\t{{.Status}}\t{{.Size}}" -sa'"
+echo  "alias dps='docker ps --format "table {{.ID}}\t{{.Names}}\t{{.Image}}\t{{.Status}}\t{{.Size}}" -sa'" >> /root/.zsh_aliases
 
 echo "export HISTFILESIZE=" >> /root/.bashrc
 echo "export HISTSIZE=" >> /root/.bashrc
+
+# install oh my zsh with bira theme
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+sed -i -- 's/robbyrussell/bira/g' /root/.zshrc
