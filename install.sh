f2e80b80c2198b7734550c8e10ea7e318d049dbc
Prime-Host <knordloh@nordloh-webdesign.de>
2018-05-04 12:12:08 +0200

    new docker and docker-compose version

--- install.sh
+++ install.sh
@@ -14,11 +14,12 @@ add-apt-repository \
    $(lsb_release -cs) \
    stable"
 
-# install docker version with rancher support
-apt-get install docker-ce=17.06.2~ce-0~ubuntu
+# install docker 
+apt-get update
+apt-get install docker-ce
 
 # install docker-compose
-curl -L https://github.com/docker/compose/releases/download/1.16.1/docker-compose-`uname -s`-`uname -m` > /usr/local/bin/docker-compose
+curl -L https://github.com/docker/compose/releases/download/1.21.2/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
 chmod +x /usr/local/bin/docker-compose
 
 # dps alias for better overview of containers
