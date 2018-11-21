209a8c683a7162edefbd88b770eb6f11161395c2
Prime Host <gregorius.stahlbart@gmail.com>
2018-11-21 16:04:34 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -19,7 +19,7 @@ apt-get update
 apt-get install docker-ce -y
 
 # install docker-compose
-curl -L https://github.com/docker/compose/releases/download/1.21.2/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
+curl -L https://github.com/docker/compose/releases/download/1.23.1/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
 chmod +x /usr/local/bin/docker-compose
 
 # dps alias for better overview of containers
