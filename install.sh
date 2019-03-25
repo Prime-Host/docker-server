11a1b16c7fc00e818276f837d55bf8150835074a
Prime Host <gregorius.stahlbart@gmail.com>
2019-03-25 15:23:24 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -5,27 +5,26 @@ apt-get update
 apt-get dits-upgrade -y
 apt-get install vim htop iftop git curl zsh -y
 
-# install docker requirments and add repo
-apt-get install linux-image-extra-$(uname -r) linux-image-extra-virtual -y
-apt-get install apt-transport-https ca-certificates curl software-properties-common -y
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-add-apt-repository \
-   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
-   $(lsb_release -cs) \
-   stable"
 
-# install docker 
-apt-get update
-apt-get install docker-ce -y
 
-# install docker-compose
-curl -L https://github.com/docker/compose/releases/download/1.23.1/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
+apt-get install docker.io -y
+curl -L https://github.com/docker/compose/releases/download/1.24.0-rc1/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
 chmod +x /usr/local/bin/docker-compose
 
-# dps alias for better overview of containers
-echo  "alias dps='docker ps --format "'"'table {{.ID}}\\\\t{{.Names}}\\\\t{{.Image}}\\\\t{{.Status}}\\\\t{{.Size}}'"'" -sa'" >> /root/.zshrc
+
+# install oh-my-zsh
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+cp ~/.oh-my-zsh/themes/bira.zsh-theme ~/.oh-my-zsh/themes/prime-host.zsh-theme \
+ && sed -i 's/%m%/%M%/g' ~/.oh-my-zsh/themes/prime-host.zsh-theme \
+ && sed -i s:~/.oh-my-zsh:\$HOME/.oh-my-zsh:g ~/.zshrc \
+ && sed -i 's/robbyrussell/prime-host/g' ~/.zshrc \
+ && echo "DISABLE_UPDATE_PROMPT=true" >> ~/.zshrc
+
+
+echo  "alias dps='docker ps --format "'"'table {{.Names}}\\\\t{{.Image}}\\\\t{{.Status}}\\\\t{{.Size}}'"'" -sa'" >> /root/.zshrc
 echo "export HISTFILESIZE=" >> /root/.bashrc
 echo "export HISTSIZE=" >> /root/.bashrc
+
 
 # install oh my zsh with custom theme
 wget https://github.com/robbyrussell/oh-my-zsh/raw/master/tools/install.sh -O - | zsh || true \
@@ -40,6 +39,8 @@ wget https://github.com/robbyrussell/oh-my-zsh/raw/master/tools/install.sh -O - 
  && cp /root/.zshrc /etc/skel/. \
  && cp /root/.vimrc /etc/skel/. \
  && chsh -s $(which zsh)
+ 
+ 
 
 #install netdata monitoring
 docker pull netdata/netdata
