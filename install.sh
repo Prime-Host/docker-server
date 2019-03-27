6e50ab2562d749f305c073680753016c6d8210fa
Prime Host <gregorius.stahlbart@gmail.com>
2019-03-27 13:34:26 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -5,8 +5,7 @@ apt-get update
 apt-get dits-upgrade -y
 apt-get install vim htop iftop git curl zsh -y
 
-
-
+# install docker and docker-compose
 apt-get install docker.io -y
 curl -L https://github.com/docker/compose/releases/download/1.24.0-rc1/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
 chmod +x /usr/local/bin/docker-compose
@@ -20,47 +19,19 @@ cp ~/.oh-my-zsh/themes/bira.zsh-theme ~/.oh-my-zsh/themes/prime-host.zsh-theme \
  && sed -i 's/robbyrussell/prime-host/g' ~/.zshrc \
  && echo "DISABLE_UPDATE_PROMPT=true" >> ~/.zshrc
 
-
+# docker ps alias and longer history
 echo  "alias dps='docker ps --format "'"'table {{.Names}}\\\\t{{.Image}}\\\\t{{.Status}}\\\\t{{.Size}}'"'" -sa'" >> /root/.zshrc
 echo "export HISTFILESIZE=" >> /root/.bashrc
 echo "export HISTSIZE=" >> /root/.bashrc
 
+# reduce swap usage
+echo "vm.swappiness=10" >> /etc/sysctl.conf
 
-# install oh my zsh with custom theme
-wget https://github.com/robbyrussell/oh-my-zsh/raw/master/tools/install.sh -O - | zsh || true \
- && cp /root/.oh-my-zsh/themes/bira.zsh-theme /root/.oh-my-zsh/themes/prime-host.zsh-theme \
- && sed -i 's/%m%/%M%/g' /root/.oh-my-zsh/themes/prime-host.zsh-theme \
- && sed -i s:/root/.oh-my-zsh:\$HOME/.oh-my-zsh:g /root/.zshrc \
- && sed -i 's/robbyrussell/prime-host/g' /root/.zshrc \
- && echo "DISABLE_UPDATE_PROMPT=true" >> /root/.zshrc \
- && echo "set encoding=utf-8" >> /root/.vimrc \
- && echo "set fileencoding=utf-8" >> /root/.vimrc \
- && cp -r /root/.oh-my-zsh /etc/skel/. \
- && cp /root/.zshrc /etc/skel/. \
- && cp /root/.vimrc /etc/skel/. \
- && chsh -s $(which zsh)
- 
- 
-
-#install netdata monitoring
-docker pull netdata/netdata
-docker run -d --name=netdata \
-  --net="webproxy" \
-  --restart always \
-  -h node002.legendary-server.de \
-  -e TZ="Europe/Berlin" \
-  -e "VIRTUAL_HOST"="stats.node002.legendary-server.de" \
-  -e "LETSENCRYPT_HOST"="stats.node002.legendary-server.de" \
-  -e "LETSENCRYPT_EMAIL"="info@nordloh-webdesign.de" \
-  -e "VIRTUAL_PORT"="19999" \
-  -e PGID=999 \
-  -p 19999:19999 \
-  -v /proc:/host/proc:ro \
-  -v /sys:/host/sys:ro \
-  -v /var/run/docker.sock:/var/run/docker.sock:ro \
-  --cap-add SYS_PTRACE \
-  --security-opt apparmor=unconfined \
-  netdata/netdata
-
-#disable swap
-swapoff -a
+# install let's encrypt and nginx container
+git clone https://github.com/evertramos/docker-compose-letsencrypt-nginx-proxy-companion.git
+cd docker-compose-letsencrypt-nginx-proxy-companion
+mv .env.sample .env
+PROXY_IP_DOCKER=$(ip addr show ens18 | awk '$1 == "inet" {gsub(/\/.*$/, "", $2); print $2}')
+sed -i "s/0.0.0.0/$PROXY_IP_DOCKER/g" .env
+sed -i "s:./nginx-data:/var/docker-data/proxy:g" .env
+./start.sh
