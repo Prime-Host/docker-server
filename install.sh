f49d56fc1c20cbfc5bfdfe5047497156e47e5db3
Prime Host <gregorius.stahlbart@gmail.com>
2019-04-09 16:38:12 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -34,6 +34,7 @@ sh -c "openssl passwd -apr1 ${P_PASSWORD} >> /var/docker-data/proxy/htpasswd/sta
 
 # install oh-my-zsh and change theme
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh | sed 's:env zsh -l::g' | sed 's:chsh -s .*$::g')"
+chsh -s /bin/zsh
 cp ~/.oh-my-zsh/themes/bira.zsh-theme ~/.oh-my-zsh/themes/prime-host.zsh-theme \
  && sed -i 's/%m%/%M%/g' ~/.oh-my-zsh/themes/prime-host.zsh-theme \
  && sed -i s:~/.oh-my-zsh:\$HOME/.oh-my-zsh:g ~/.zshrc \
