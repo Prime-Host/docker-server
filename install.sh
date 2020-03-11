9be94f7affe889e2b36c2d8743d7c649df33097c
root <root@vmd37390.contaboserver.net>
2020-03-11 11:34:39 +0100

    new version with traefik

--- install.sh
+++ install.sh
@@ -1,38 +1,33 @@
 #!/bin/bash
 
-# install basiscs
+# update & install basiscs
 apt-get update
 apt-get dist-upgrade -y
-apt-get install vim htop iftop curl zsh borgbackup -y
+apt-get install vim htop iftop curl wget zsh borgbackup -y
 
 # install docker and docker-compose
 apt-get install docker.io -y
-curl -L "https://github.com/docker/compose/releases/download/1.24.1/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+curl -L "https://github.com/docker/compose/releases/download/1.25.4/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 chmod +x /usr/local/bin/docker-compose
 systemctl enable docker
+docker network create web
 
 # change hostname, reduce swap usage and create folders
 echo ${P_DOMAIN} > /etc/hostname 
 echo "vm.swappiness=10" >> /etc/sysctl.conf
-mkdir -p /var/docker-data/env /var/docker-data/container
+mkdir -p /var/docker/env /var/docker/container /var/docker/traefik
 
-# install let's encrypt and nginx container
-git clone https://github.com/evertramos/docker-compose-letsencrypt-nginx-proxy-companion.git
-cd docker-compose-letsencrypt-nginx-proxy-companion
-mv .env.sample .env
-PROXY_IP_DOCKER=$(ip addr show eth0 | awk '$1 == "inet" {gsub(/\/.*$/, "", $2); print $2}')
-sed -i "s/0.0.0.0/$PROXY_IP_DOCKER/g" .env
-sed -i "s:./nginx-data:/var/docker-data/proxy:g" .env
-./start.sh
+# create env files for traefik and netdata
+echo "P_USER=admin" > /var/docker/env/traefik.${P_DOMAIN}.env
+{ echo P_CRYPT && openssl passwd -apr1 ${P_PASSWORD}; } | paste -d"=" -s >> /var/docker/env/traefik.${P_DOMAIN}.env
 
-# install netdata container
-cd ../netdata
+# install traefik and netdata
+touch /var/docker/traefik/acme.json
+chmod 600 /var/docker/traefik/acme.json
+cd /root/docker/docker-server
+docker-compose pull traefik netdata
 export P_PGID=$(grep docker /etc/group | cut -d ':' -f 3)
-docker-compose pull && docker-compose -p ${P_DOMAIN} up -d
-
-# Secure netdata with authentication
-sh -c "echo -n 'netdata:' >> /var/docker-data/proxy/htpasswd/stats.${P_DOMAIN}"
-sh -c "openssl passwd -apr1 ${P_PASSWORD} >> /var/docker-data/proxy/htpasswd/stats.${P_DOMAIN}"
+env $(cat /var/docker/env/traefik.${P_DOMAIN}.env) docker-compose -p traefik.${P_DOMAIN} up -d
 
 # install oh-my-zsh and change theme
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)" "" --unattended
@@ -44,12 +39,7 @@ cp ~/.oh-my-zsh/themes/bira.zsh-theme ~/.oh-my-zsh/themes/prime-host.zsh-theme \
  && sed -i 's/# DISABLE_UPDATE_PROMPT="true"/DISABLE_UPDATE_PROMPT="true"/g' ~/.zshrc \
  && sed -i 's/plugins=(git)/plugins=(git git-extras docker docker-compose)/g' ~/.zshrc 
 
-
 # docker ps alias and longer history
-echo  "alias dps='docker ps --format "'"'table {{.Names}}\\\\t{{.Image}}\\\\t{{.Status}}\\\\t{{.Size}}'"'" -sa'" >> /root/.zshrc
+echo "alias dps='docker ps --format "'"'table {{.Names}}\\\\t{{.Image}}\\\\t{{.Status}}\\\\t{{.Size}}'"'" -sa'" >> /root/.zshrc
 echo "export HISTFILESIZE=" >> /root/.bashrc
 echo "export HISTSIZE=" >> /root/.bashrc
-
-echo "System will now reboot"
-sleep 3s
-reboot
