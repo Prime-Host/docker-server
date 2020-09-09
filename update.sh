d71c1f071a3655f7a7abe432ab0f99aff1a57564
root <root@vmd37473.contaboserver.net>
2020-09-09 14:36:51 +0200

    new update script

--- update.sh
+++ update.sh
@@ -1,19 +1,13 @@
 #!/bin/bash
+SERVER=$(cat /etc/hostname)
 
-# update & install basiscs
+# update & install packages
 apt-get update
 apt-get dist-upgrade -y
 
-# create env files for traefik and netdata
-mkdir -p /var/docker/env /var/docker/container /var/docker/traefik
-echo "P_USER=admin" > /var/docker/env/traefik.${P_DOMAIN}.env
-{ echo P_CRYPT && openssl passwd -apr1 ${P_PASSWORD}; } | paste -d"=" -s >> /var/docker/env/traefik.${P_DOMAIN}.env
-
-# install traefik and netdata
-touch /var/docker/traefik/acme.json
-chmod 600 /var/docker/traefik/acme.json
+# update traefik, netdata & phpmyadmin
 cd /root/docker/docker-server
+git pull origin master
 export P_PGID=$(grep docker /etc/group | cut -d ':' -f 3)
-docker pull traefik
-docker pull netdata/netdata
-env $(cat /var/docker/env/traefik.${P_DOMAIN}.env) docker-compose -p traefik.${P_DOMAIN} up -d
+env $(cat /var/docker/env/traefik.${SERVER}.env) docker-compose pull
+env $(cat /var/docker/env/traefik.${SERVER}.env) docker-compose -p traefik.${SERVER} up -d
