4d8f5cde873c8980f81395d913291cf0b3098ff9
Nordloh Webdesign <gregorius.stahlbart@gmail.com>
2021-08-26 14:40:18 +0200

    Update update.sh

--- update.sh
+++ update.sh
@@ -9,5 +9,5 @@ SERVER=$(cat /etc/hostname)
 cd /root/docker/docker-server
 git pull origin master
 export P_PGID=$(grep docker /etc/group | cut -d ':' -f 3)
-env $(cat /var/docker/env/traefik.${SERVER}.env) docker-compose pull
-env $(cat /var/docker/env/traefik.${SERVER}.env) docker-compose -p traefik.${SERVER} up -d
+env $(cat /var/docker-data/env/traefik.${SERVER}.env) docker-compose pull
+env $(cat /var/docker-data/env/traefik.${SERVER}.env) docker-compose -p traefik.${SERVER} up -d
