9ea277ef7a93bdeab5f614bfefaf04ace9b7977f
Nordloh Webdesign <gregorius.stahlbart@gmail.com>
2021-08-26 14:39:12 +0200

    Update update.sh

--- update.sh
+++ update.sh
@@ -2,12 +2,11 @@
 SERVER=$(cat /etc/hostname)
 
 # update & install packages
-apt-get update
-apt-get dist-upgrade -y
+#apt-get update
+#apt-get dist-upgrade -y
 
 # update traefik, netdata & phpmyadmin
 cd /root/docker/docker-server
-git reset --hard
 git pull origin master
 export P_PGID=$(grep docker /etc/group | cut -d ':' -f 3)
 env $(cat /var/docker/env/traefik.${SERVER}.env) docker-compose pull
