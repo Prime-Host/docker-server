7d85f9b5dc88b867875f4391b993f2d62b0c5b91
root <root@vmd37473.contaboserver.net>
2021-09-09 12:09:49 +0200

    update netdata

--- netdata/update-netdata.sh
+++ netdata/update-netdata.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 SERVER=$(cat /etc/hostname)
+export P_PGID=$(grep docker /etc/group | cut -d ':' -f 3)
 
 cd /root/docker/docker-server/netdata
-git pull origin master
-export P_PGID=$(grep docker /etc/group | cut -d ':' -f 3)
 env $(cat /var/docker-data/env/traefik.${SERVER}.env) docker-compose pull
 env $(cat /var/docker-data/env/traefik.${SERVER}.env) docker-compose -p stats.${SERVER} up -d
