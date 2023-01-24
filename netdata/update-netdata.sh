4023fffb62be7a5188959324cca0d194354fad2a
root <root@s2.nordloh.eu>
2023-01-24 14:31:01 +0000

    new volume path

--- netdata/update-netdata.sh
+++ netdata/update-netdata.sh
@@ -3,5 +3,5 @@ SERVER=$(cat /etc/hostname)
 export P_PGID=$(grep docker /etc/group | cut -d ':' -f 3)
 
 cd /root/docker/docker-server/netdata
-env $(cat /var/docker-data/env/traefik.${SERVER}.env) docker-compose pull
-env $(cat /var/docker-data/env/traefik.${SERVER}.env) docker-compose -p stats.${SERVER} up -d
+env $(cat /docker/env/traefik.${SERVER}.env) docker-compose pull
+env $(cat /docker/env/traefik.${SERVER}.env) docker-compose -p stats.${SERVER} up -d
